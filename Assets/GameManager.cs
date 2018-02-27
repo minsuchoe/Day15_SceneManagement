824dc2953a3c9cd542a87bfe9b0db3b57c93fbc9
minsu choe <choi.minsoo.msc@gmail.com>
2018-02-27 15:45:26 +0900

    Day15_SceneManagement

---
+++ Assets/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;      //Scene 로딩시 필요
+
+public class GameManager : MonoBehaviour {
+    public static GameManager instance = null;
+    public int level = 1;
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != null)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+
+        InitGame();
+    }
+
+    private void InitGame()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKeyDown(KeyCode.L))
+            SceneManager.LoadScene("Lobby");
+        if (Input.GetKeyDown(KeyCode.R))
+            SceneManager.LoadScene("Room");
+        if (Input.GetKeyDown(KeyCode.P))
+            SceneManager.LoadScene("Play");
+    }
+}
